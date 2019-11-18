a63d8537b8b4e6bd1207dd00232da12b2b63d45e
Juanferca <juanferca@hotmail.com>
2019-11-18 11:54:02 +0100

    algunos cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,89 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import { Form, HasError, AlertError } from 'vform';
+import VueProgressBar  from 'vue-progressbar';
+import Swal from 'sweetalert2';
+
+window.swal = Swal;
+
+
+const toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.toast = toast;
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.5s',
+    opacity: '0.8s',
+    termination: 600 
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+};
+
+Vue.use(VueProgressBar, options);
+
+
+import VueRouter from 'vue-router'; 
+
+Vue.use(VueRouter);
+
+let routes = [
+
+    { path: '/dashboard', component:  require('./components/Dashboard.vue').default },
+    { path: '/developer', component:  require('./components/Developer.vue').default },
+    { path: '/users', component:  require('./components/Users.vue').default },
+    { path: '/profile', component:  require('./components/Profile.vue').default }
+  ];
+
+  const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+  });
+
+  Vue.filter('upText', function(text){
+      return text.charAt(0).toUpperCase() + text.slice(1);
+  });
+
+  Vue.filter('myDate', function(created) {
+    return moment().format('MMMM Do YYYY');
+
+  });
+
+  window.Fire = new Vue();
+
+  Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+
+
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,4 +112,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
+    
 });
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,222 @@
+<template>
+    <div class="container">
+      <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title p-2">User Table</h3>
+                 <div class="card-tools">
+                     <button class="btn btn-success" @click="newModal">Add new <i class="fas fa-user-plus fa-fw ml-2"></i></button>
+                
+                 </div>
+               </div>
+                        <!-- /.card-header -->
+                      <div class="card-body table-responsive">
+                          <table class="table table-hover">
+                               <thead>
+                                 <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                     <th>Email</th>
+                                     <th>Type</th>
+                                     <th>Registered At</th>
+                                    <th>Modify</th>
+                                </tr>
+                               </thead>
+                             <tbody>
+                               <tr v-for="user in users" :key="user.id">
+                                  <td>{{user.id}}</td>
+                                 <td>{{user.name}}</td>
+                                 <td>{{user.email}}</td>
+                                 <td>{{user.type | upText }}</td>
+                                 <td>{{user.created_at | myDate}}</td>
+                                 <td>
+                                     <a href="#" @click="editModal(user)">
+                                         <i class="fa fa-edit mr-1 green"></i>
+                                    </a>
+                                    <a href="#" @click="deleteUser(user.id)">
+                                        <i class="fa fa-trash ml-1 red"></i>
+                                    </a>
+                                 </td>
+                               </tr>     
+                             </tbody>
+                          </table>
+                         </div>
+              <!-- /.card-body -->
+             </div>
+            <!-- /.card -->
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNew" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" v-show="!editmode" id="addNew">Add New</h5>
+                <h5 class="modal-title" v-show="editmode" id="addNew">Update User's Info</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form @submit.prevent="editmode ? updateUser() : createUser()">
+              <div class="modal-body">
+                <div class="form-group">
+                 <input v-model="form.name" type="text" name="name" placeholder="Name"
+                   class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                 <has-error :form="form" field="name"></has-error>
+               </div>
+                 <div class="form-group">
+                 <input v-model="form.email" type="email" name="Email" placeholder="Email Address"
+                   class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                 <has-error :form="form" field="email"></has-error>
+               </div>
+                 <div class="form-group">
+                <input v-model="form.bio" type="text" name="bio" placeholder="Short bio for user (Optional)"
+                   class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }">
+                 <has-error :form="form" field="bio"></has-error>
+               </div>
+               <div class="form-group">
+                   <select name="type" v-model="form.type" id="type" class="form-control" :class="{'is-invalid' : form.errors.has('type') }">
+                       <option value="">Select User Role</option>
+                       <option value="admin">Admin</option>
+                       <option value="user">Standard User</option>
+                       <option value="author">Author</option>               
+                   </select>
+                   <has-error :form="form" field="type"></has-error>
+               </div>
+                 <div class="form-group">
+                <input v-model="form.password" type="text" name="password" placeholder="Password"
+                   class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">   
+                 <has-error :form="form" field="password"></has-error>
+               </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button v-show="editmode" type="submit" class="btn btn-success">Update</button>
+                <button v-show="!editmode" type="submit" class="btn btn-primary">Create</button>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              editmode: false,
+              users: {},
+                form: new Form({
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+
+                }) 
+            }
+        }, 
+        methods: {
+
+          updateUser(id) {
+            this.form.put('api/user/' + this.form.id)
+            .then(() => {
+                $('#addNew').modal('hide');
+                swal.fire(
+                        'Updated',
+                        'Information has been updated.',
+                        'success'
+                      )
+                       this.$Progress.finish();
+                       Fire.$emit('AfterCreate');
+            })
+            .catch(() => {
+              this.$Progress.fail();
+
+            })
+
+          },
+             editModal(user) {
+             this.editmode = true;
+             this.form.reset();
+             $('#addNew').modal('show');
+             this.form.fill(user);
+
+         },
+             newModal() {
+             this.editmode = false;
+             this.form.reset();
+             $('#addNew').modal('show');
+
+         },
+         deleteUser(id) {
+                 swal.fire({
+                 title: 'Are you sure?',
+                 text: "You won't be able to revert this!",
+                 icon: 'warning',
+                 showCancelButton: true,
+                 confirmButtonColor: '#3085d6',
+                 cancelButtonColor: '#d33',
+                 confirmButtonText: 'Yes, delete it!'  
+               }).then((result) => {
+               //Send request to the server
+                    if(result.value) {
+                    this.form.delete('api/user/' + id)
+                      .then(() => {
+                         swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )
+                      Fire.$emit('AfterCreate');
+                      }).catch(() => {
+                        swal("Failed" , "There was something wronge.", "warning");
+                      });
+                    }
+                 })
+
+               },
+               loadUsers(){
+                   axios.get('api/user').then( ({ data }) => (this.users = data.data));
+      
+               },
+                createUser() {
+                  console.log('Creando')
+                    this.$Progress.start();
+                    this.form.post('api/user')
+                    
+                      .then(() => {
+
+                           Fire.$emit('AfterCreate');
+                           $('#addNew').modal('hide');
+
+                           toast.fire({
+                           icon: 'success',
+                           title: 'User Create is successfully'                            
+                    })
+                    this.$Progress.finish();
+
+                  })
+                  .catch(() => {
+
+                      })
+                 
+               }
+
+
+        },
+        created() {
+          this.loadUsers();
+          Fire.$on('AfterCreate', () => {
+            this.loadUsers();
+          })
+         // setInterval(() => this.loadUsers(),3000);
+        }     
+    }
+</script>
